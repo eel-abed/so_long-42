9792ae467df7f502cf99df8b1a79c8ff1b1f7df3
eel-abed <eli_elabed@hotmail.fr>
2024-05-06 12:41:50 -0400

    test avec 2 obs

--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,7 +6,7 @@
 /*   By: eel-abed <eel-abed@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 15:36:19 by eel-abed          #+#    #+#             */
-/*   Updated: 2024/05/04 19:58:04 by eel-abed         ###   ########.fr       */
+/*   Updated: 2024/05/06 12:41:39 by eel-abed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,8 @@ static void ft_error(void)
 void ft_hook(void* param)
 {
 	mlx_t* mlx = param;
-	int new_x = img->instances[0].x;
-	int new_y = img->instances[0].y;
+	unsigned int new_x = img->instances[0].x;
+	unsigned int new_y = img->instances[0].y;
 
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx);
@@ -45,7 +45,16 @@ void ft_hook(void* param)
 	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
 		new_x += 5;
 
-	// Check if the new position is within the window boundaries
+	// Check if the new position collides with any of the obstacles
+	for (size_t i = 0; i < obstacle->count; i++) {
+        if (!(new_x + img->width < (unsigned int)obstacle->instances[i].x || new_x > (unsigned int)obstacle->instances[i].x + 32 ||
+              new_y + img->height < (unsigned int)obstacle->instances[i].y || new_y > (unsigned int)obstacle->instances[i].y + 32)) {
+            // New position collides with an obstacle, do not move
+            return;
+        }
+    }
+
+	// Check if it is within the window boundaries
 	if (new_x >= 0 && new_x <= WINDOW_WIDTH && new_y >= 0 && new_y <= WINDOW_HEIGHT) {
 		img->instances[0].x = new_x;
 		img->instances[0].y = new_y;
@@ -79,15 +88,36 @@ int	main(void)
 		}
 	}
 
-	// Display an instance of the image
-	if (mlx_image_to_window(mlx, obstacle, 100, 0) < 0)
+	// Allocate space for two instances
+	obstacle->instances = malloc(2 * sizeof(*obstacle->instances));
+	if (!obstacle->instances)
 		ft_error();
+
+	// Set the position of the first instance
+	obstacle->instances[0].x = 100;
+	obstacle->instances[0].y = 0;
+
+	// Set the position of the second instance
+	obstacle->instances[1].x = 200;
+	obstacle->instances[1].y = 200;
+
+	// Update the count of instances
+	obstacle->count = 2;
+
+	// Display an instance of the obstacle
+	if (mlx_image_to_window(mlx, obstacle, obstacle->instances[0].x, obstacle->instances[0].y) < 0)
+		ft_error();
+
+	// Display the second instance of the obstacle
+	if (mlx_image_to_window(mlx, obstacle, obstacle->instances[1].x, obstacle->instances[1].y) < 0)
+		ft_error();
+
 	// Display an instance of the image
 	if (mlx_image_to_window(mlx, img, 0, 0) < 0)
-        ft_error();
+		ft_error();
 
 	mlx_loop_hook(mlx, ft_hook, mlx);
-	
+		
 	mlx_loop(mlx);
 	
 
