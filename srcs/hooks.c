e1d78a9ee874161490ab275ef065494a81d4d8e6
eel-abed <eli_elabed@hotmail.fr>
2024-05-28 19:12:59 -0400

    ok sortie bloqué ah moins recupere colectible

--- srcs/hooks.c
+++ srcs/hooks.c
@@ -6,7 +6,7 @@
 /*   By: eel-abed <eel-abed@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 15:40:33 by eel-abed          #+#    #+#             */
-/*   Updated: 2024/05/28 13:32:19 by eel-abed         ###   ########.fr       */
+/*   Updated: 2024/05/28 19:11:01 by eel-abed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 
 unsigned int total_movements = 0; // Global variable to keep track of total movements
+unsigned int collectibles_taken = 0; // Global variable to keep track of collectibles taken
 
 void ft_hook(void* param)
 {
@@ -44,7 +45,36 @@ void ft_hook(void* param)
 			return;
 		}
 	}
+	// Check if the new position collides with any of the collectibles
+	for (size_t i = 0; i < collectible->count; i++) {
+		if (!(new_x + player->width < (unsigned int)collectible->instances[i].x || new_x > (unsigned int)collectible->instances[i].x + 64 ||
+			  new_y + player->height < (unsigned int)collectible->instances[i].y || new_y > (unsigned int)collectible->instances[i].y + 64)) {
+			// New position collides with a collectible, remove the collectible
+			collectible->instances[i].x = -1;
+			collectible->instances[i].y = -1;
+            collectibles_taken++; // Increment the number of collectibles taken
+
+			return;
+		}
+	}
+
+	// Check if the new position collides with the exit
+    if (!(new_x + player->width < (unsigned int)sortie->instances[0].x || new_x > (unsigned int)sortie->instances[0].x + 64 ||
+          new_y + player->height < (unsigned int)sortie->instances[0].y || new_y > (unsigned int)sortie->instances[0].y + 64)) {
+        // New position collides with the exit, check if all collectibles have been taken
+        if (collectibles_taken == collectible->count) {
+            // All collectibles have been taken, allow the player to exit
+            printf("You've collected all collectibles and can now exit!\n");
+			mlx_close_window(mlx);
+        } else {
+            // Not all collectibles have been taken, do not allow the player to exit
+            printf("You need to collect all collectibles before you can exit!\n");
+            return;
+        }
+    }
 	
+
+
 	// Check if it is within the window boundaries
 	if (new_x >= 0 && new_x <= (unsigned int)mlx->width && new_y >= 0 && new_y <= (unsigned int)mlx->height) {
 		player->instances[0].x = new_x;
