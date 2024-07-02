77fba9c81448f83c78b06d0dfc898b348921357e
eel-abed <eli_elabed@hotmail.fr>
2024-07-02 15:34:05 -0400

    map.c prend mtn game assets

--- srcs/map.c
+++ srcs/map.c
@@ -6,7 +6,7 @@
 /*   By: eel-abed <eel-abed@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 15:39:07 by eel-abed          #+#    #+#             */
-/*   Updated: 2024/06/30 13:16:38 by eel-abed         ###   ########.fr       */
+/*   Updated: 2024/07/02 15:33:39 by eel-abed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,18 +32,14 @@ void	get_window_dimensions(char *map_name,
 	close(fd);
 }
 
-void read_map(char *map_name, GameAssets* game_assets, Dimensions *dim)
+void	read_map(char *map_name, GameAssets *game_assets, Dimensions *dim)
 {
-	mlx_image_t *player = game_assets->player;
-	mlx_image_t *obstacle = game_assets->obstacle;
-	mlx_image_t *collectible = game_assets->collectible;
-	mlx_image_t *sortie = game_assets->sortie;
-	int	fd;
+	int			fd;
 	fd = open(map_name, O_RDONLY);
 	if (fd == -1)
 	{
 		printf("Could not open file %s\n", map_name);
-		return;
+		return ;
 	}
 	char ch;
 	int x = 0, y = 0;
@@ -76,47 +72,47 @@ void read_map(char *map_name, GameAssets* game_assets, Dimensions *dim)
 			map_data[row][col] = ch;
 			if (ch == '1')
 			{
-				obstacle->instances[obstacle->count].x = x * 64;
-				obstacle->instances[obstacle->count].y = y * 64;
-				obstacle->count++;
+				game_assets->obstacle->instances[game_assets->obstacle->count].x = x * 64;
+				game_assets->obstacle->instances[game_assets->obstacle->count].y = y * 64;
+				game_assets->obstacle->count++;
 			}
 			else if (ch == 'P')
 			{
-				player->instances[0].x = x * 64 + 5;
-				player->instances[0].y = y * 64 + 5;
-				player->count++;
+				game_assets->player->instances[0].x = x * 64;
+				game_assets->player->instances[0].y = y * 64;
+				game_assets->player->count++;
 				player_start_x = x;
 				player_start_y = y;
 			}
 			else if (ch == 'C')
 			{
-				collectible->instances[collectible->count].x = x * 64;
-				collectible->instances[collectible->count].y = y * 64;
-				collectible->count++;
+				game_assets->collectible->instances[game_assets->collectible->count].x = x * 64;
+				game_assets->collectible->instances[game_assets->collectible->count].y = y * 64;
+				game_assets->collectible->count++;
 				collectible_total++;
 			}
 			else if (ch == 'E')
 			{
-				sortie->instances[0].x = x * 64;
-				sortie->instances[0].y = y * 64;
-				sortie->count++;
+				game_assets->sortie->instances[0].x = x * 64;
+				game_assets->sortie->instances[0].y = y * 64;
+				game_assets->sortie->count++;
 			}
 			col++;
 			x++;
 		}
 	}
 	close(fd);
-	if (player->count != 1)
+	if (game_assets->player->count != 1)
 	{
 		printf("Error: Map must contain exactly one player start position.\n");
 		ft_error();
 	}
-	if (sortie->count != 1)
+	if (game_assets->sortie->count != 1)
 	{
 		printf("Error: Map must contain exactly one exit.\n");
 		ft_error();
 	}
-	if (collectible->count < 1)
+	if (game_assets->collectible->count < 1)
 	{
 		printf("Error: Map must contain at least one collectible.\n");
 		ft_error();
